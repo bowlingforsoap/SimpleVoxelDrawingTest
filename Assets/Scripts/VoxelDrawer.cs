ab3b0b71ff425274fca9ee550dabda0788dc3914
Vadym Strelchenko <bowlingforsoap@mail.ru>
2018-08-07 14:11:08 +1200

    spawning voxels of given voxelSize; not yet in the grid

---
+++ Assets/Scripts/VoxelDrawer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VoxelDrawer : MonoBehaviour {
+	public float voxelSize;
+	[Tooltip("A GameObject with scale Vector3.one.")]
+	public GameObject voxelPrefab;
+
+	private SteamVR_TrackedObject trackedObj;
+    private SteamVR_Controller.Device Controller
+    {
+        get { return SteamVR_Controller.Input((int)trackedObj.index); }
+    }
+
+	void Awake () {
+        trackedObj = GetComponent<SteamVR_TrackedObject>();
+	}
+
+	// Use this for initialization
+	void Start () {
+		voxelPrefab.transform.localScale = new Vector3(voxelSize, voxelSize, voxelSize);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Controller.GetHairTrigger()) // Hair-Trigger press
+		{
+			Instantiate(voxelPrefab, transform.position, Quaternion.identity);
+		}
+	}
+}
