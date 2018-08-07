c8ce2d4f0b7113a04a2bb93cd5190adb71e5da7f
Vadym Strelchenko <bowlingforsoap@mail.ru>
2018-08-07 14:55:25 +1200

    drawing voxels in a "grid"

--- Assets/Scripts/VoxelDrawer.cs
+++ Assets/Scripts/VoxelDrawer.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class VoxelDrawer : MonoBehaviour {
+	[Range(0.0000001f,1f)]
 	public float voxelSize;
 	[Tooltip("A GameObject with scale Vector3.one.")]
 	public GameObject voxelPrefab;
@@ -12,6 +13,8 @@ public class VoxelDrawer : MonoBehaviour {
     {
         get { return SteamVR_Controller.Input((int)trackedObj.index); }
     }
+
+	private Dictionary<Vector3, bool> voxelData = new Dictionary<Vector3, bool>(10000);
 
 	void Awake () {
         trackedObj = GetComponent<SteamVR_TrackedObject>();
@@ -24,9 +27,38 @@ public class VoxelDrawer : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (Controller.GetHairTrigger()) // Hair-Trigger press
+		if (Controller.GetHairTrigger()) // true, if even lightly pressed
 		{
-			Instantiate(voxelPrefab, transform.position, Quaternion.identity);
+			DrawVoxel(transform.position);
 		}
+	}
+
+
+	/// <summary>Returns true, if a new voxel was added, false if the voxel was already present at the given position.</summary>
+	private bool DrawVoxel(Vector3 position) {
+		Vector3 voxelIndex;
+		bool alreadyPresent;
+
+		voxelIndex = PositionToVoxelIndex(position);
+		voxelData.TryGetValue(voxelIndex, out alreadyPresent);
+
+		if (!alreadyPresent) {
+			voxelData.Add(voxelIndex, true);
+			Instantiate(voxelPrefab, voxelIndex, Quaternion.identity);
+		}
+
+		return alreadyPresent;
+	}
+
+	private Vector3 PositionToVoxelIndex(Vector3 position) {
+		Vector3 voxelIndex;
+
+		voxelIndex = position / voxelSize;
+		voxelIndex.x = Mathf.Floor(voxelIndex.x);
+		voxelIndex.y = Mathf.Floor(voxelIndex.y);
+		voxelIndex.z = Mathf.Floor(voxelIndex.z);
+		voxelIndex *= voxelSize;
+
+		return voxelIndex;
 	}
 }
