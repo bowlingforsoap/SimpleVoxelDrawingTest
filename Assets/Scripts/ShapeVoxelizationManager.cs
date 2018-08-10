287662cf08b48403ae3b2d4df86582a173107122
Vadym Strelchenko <bowlingforsoap@mail.ru>
2018-08-10 15:34:19 +1200

    Shape voxelization WIP

---
+++ Assets/Scripts/ShapeVoxelizationManager.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class ShapeVoxelizationManager : MonoBehaviour
+{
+	[Tooltip("Parent for all voxels of the voxelized shape.")]
+	public Transform voxelizedShapeVoxelsParent;
+
+	private GameObject shape;
+	[SerializeField]
+    public GameObject Shape
+    {
+        get { return shape; }
+
+        set
+        {
+            shape = value;
+            SetShapeBounds();
+        }
+    }
+
+    private Bounds shapeBounds;
+	private Dictionary<Vector3, bool> voxelizedShapeMeta = new Dictionary<Vector3, bool>(10000);
+	private Dictionary<Vector3, GameObject> voxelizedShape = new Dictionary<Vector3, GameObject>(10000);
+
+    // Use this for initialization
+    void Start()
+    {
+        if (Shape != null)
+        {
+            SetShapeBounds();
+        }
+
+		StartCoroutine(VoxelizeShape());
+    }
+
+    private void SetShapeBounds()
+    {
+        shapeBounds = Shape.GetComponent<MeshFilter>().mesh.bounds;
+    }
+
+	private IEnumerator VoxelizeShape() {
+		Vector3 startPoint = shapeBounds.min;
+		float voxelSize;
+
+		//while (true) { // Wait until VoxelDrawer initialized
+		//	try {
+				voxelSize = VoxelDrawer.GetVoxelDrawer().voxelSize;
+		//		break;
+		//	} catch (NullReferenceException) {}
+		//}
+
+		Vector3 voxelIndex;
+		for (int i = 0; i < shapeBounds.size.x; i++) {
+			for (int j = 0; j < shapeBounds.size.y; j++) {
+				for (int k = 0; k < shapeBounds.size.z; k++) {
+					yield return null;
+
+					voxelIndex = Utils.PositionToVoxelIndex(startPoint + new Vector3(voxelSize * i, voxelSize * j, voxelSize * k), voxelSize);
+
+					if (VoxelInsideShape(voxelIndex, voxelSize)) {
+						voxelizedShape.Add(voxelIndex, VoxelDrawer.GetVoxelDrawer().DrawVoxel(voxelIndex, voxelizedShapeVoxelsParent));
+					}
+				}
+			}
+		}
+	}
+
+	private bool VoxelInsideShape(Vector3 voxelIndex, float voxelSize) {
+		return Physics.CheckSphere(voxelIndex, voxelSize / 2f, Shape.layer);
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
--- Assets/Scripts/VoxelDrawer.cs
+++ Assets/Scripts/VoxelDrawer.cs
@@ -27,6 +27,8 @@ public class VoxelDrawer : MonoBehaviour {
 	[SerializeField]
 	private Material brushTipCenterMaterial;
 
+	private static VoxelDrawer voxelDrawer;
+
 	void Awake () {
         trackedObj = GetComponent<SteamVR_TrackedObject>();
 		trackedController = GetComponent<SteamVR_TrackedController>();
@@ -34,6 +36,8 @@ public class VoxelDrawer : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+		voxelDrawer = this;
+
 		voxelPrefab.transform.localScale = new Vector3(voxelSize, voxelSize, voxelSize);
 
 		StartCoroutine(InitializationBrushTip());
@@ -47,7 +51,7 @@ public class VoxelDrawer : MonoBehaviour {
 
 			if (Controller.GetHairTrigger()) // true, if even lightly pressed
 			{
-				DrawVoxel(brushTip.position);
+				DrawVoxel(brushTip.position, voxelsParent);
 			}
 
 			if (Controller.GetTouch(SteamVR_Controller.ButtonMask.Axis0)) {
@@ -92,22 +96,20 @@ public class VoxelDrawer : MonoBehaviour {
 	}
 
 	/// <summary>Returns true, if a new voxel was added, false if the voxel was already present at the given position.</summary>
-	private bool DrawVoxel(Vector3 position) {
-		bool alreadyPresent = false;
+	public GameObject DrawVoxel(Vector3 position, Transform voxelsParent) {
 		Vector3 voxelIndex;
 		GameObject voxel = null;
 
 		voxelIndex = Utils.PositionToVoxelIndex(position, voxelSize);
 		try {
 			voxel = voxelData[voxelIndex];
-			alreadyPresent = true;
 		} catch (KeyNotFoundException) {}
 
 		if (voxel == null) {
 			voxelData.Add(voxelIndex, Instantiate(voxelPrefab, voxelIndex, Quaternion.identity, voxelsParent));
 		}
 
-		return alreadyPresent;
+		return voxel;
 	}
 
 	private void Purge(Vector3 voxelPosition) {
@@ -139,41 +141,7 @@ public class VoxelDrawer : MonoBehaviour {
 		return erased;
 	}
 
-	private static class Utils {
-		/// <summary>Corner points + center.</summary>
-		public static Vector3[] GetBoundingBoxPoints(Vector3 center, float size) {
-			Vector3[] points = new Vector3[9];
-			Vector3 cornerPoint;
-			int count = 0;
-			points[count++] = center;
-			float halfSize = size / 2f;
-
-			for (int i = 0; i < 2; i++) {
-				for (int j = 0; j < 2; j++) {
-					float iIndexMagic = -1 * i * 2 + 1;
-					float jIndexMagic = -1 * j * 2 + 1;
-					cornerPoint = new Vector3(center.x + halfSize * iIndexMagic, center.y + halfSize, center.z + halfSize * jIndexMagic);
-					
-					points[count++] = cornerPoint; // Upper corner point
-					points[count++] = new Vector3(cornerPoint.x, cornerPoint.y - size, cornerPoint.z); // Lower corner point
-				}
-			}
-
-			return points;
-		}
-
-		public static Vector3 PositionToVoxelIndex(Vector3 position, float voxelSize) {
-			Vector3 voxelIndex;
-
-			voxelIndex = position / voxelSize;
-			voxelIndex.x = Mathf.Floor(voxelIndex.x);
-			voxelIndex.y = Mathf.Floor(voxelIndex.y);
-			voxelIndex.z = Mathf.Floor(voxelIndex.z);
-			voxelIndex *= voxelSize;
-
-			voxelIndex += new Vector3(voxelSize/2f, voxelSize/2f, voxelSize/2f); // Move half a voxel left
-
-			return voxelIndex;
-		}
+	public static VoxelDrawer GetVoxelDrawer() {
+		return voxelDrawer;
 	}
 }
